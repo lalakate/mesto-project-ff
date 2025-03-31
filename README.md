d653a50204efba5b33c05878ae997ab80640575b
Katsiaryna Kapylova <lologusenica@yandex.ru>
2025-03-31 12:18:11 +0300

    add create and delete card functions

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Проектная работа Mesto
+# Проектная работа Место
 
+This project was created to consolidate the skills in the JavaScript basics
+
+## Used stack
+- HTML
+- CSS
+- JavaScript
+
+## Project link
+```bash
+https://github.com/lalakate/mesto-project-ff.git
+```
--- scripts/cards.js
+++ scripts/cards.js
@@ -24,3 +24,32 @@ const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     }
 ];
+
+const createCard = (card, deleteCard) => {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
+
+  deleteButton.addEventListener('click', () => {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;
+}
+
+const deleteCard = (cardElement) => {
+  cardElement.remove();
+};
+
+const places__list = document.querySelector('.places__list');
+initialCards.forEach(card => {
+  const cardElement = createCard(card, deleteCard);
+  places__list.append(cardElement);
+});
